789ab0c5aca1a3ddddb32337b4736de0e21052d6
Deyan Yordanov <deyanyordanov@hotmail.com>
2025-10-16 15:25:32 +0300

    new one

--- main.tf
+++ main.tf
@@ -35,20 +35,14 @@ module "blog_autoscaling" {
 
   name = "blog"
 
-  launch_template = {
-    name = "blog-lt"
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  security_groups = [module.blog_sg.security_group_id]
 
-    image_id      = data.aws_ami.app_ami.id
-    instance_type = var.instance_type
-    security_groups = [module.blog_sg.security_group_id]
-  }
-
-  autoscaling_group = {
-    min_size           = 1
-    max_size           = 2
-    vpc_zone_identifier = module.blog_vpc.public_subnets
-    target_group_arns   = module.blog_alb.target_group_arns
-  }
+  min_size           = 1
+  max_size           = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
 }
 
 
